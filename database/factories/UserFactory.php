2de2bb5aeacd658ae84b2678dcc018a2acd88204
Stokker <stokkerma@gmail.com>
2026-05-14 15:28:42 -0500

    seeders optimizados para laravel cloud

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,8 +27,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker()->name(),
+            'email' => $this->faker()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'two_factor_secret' => null,
